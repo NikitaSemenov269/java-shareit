84a3d83bd5000d73bdcd8e58b1db76259e497c6b
Nikita Semenov <nik.269@yandex.ru>
2025-09-16 18:25:11 +0300

    Удаление лишних файлов попавших в проект по ошибке.

--- Practicum/java-filmorate-main/src/main/java/ru/yandex/practicum/filmorate/dao/interfaces/LikeRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.practicum.filmorate.dao.interfaces;
-
-public interface LikeRepository {
-
-    void addLike(Long filmId, Long userId);
-
-    void removeLike(Long filmId, Long userId);
-}
