9142ea540a002c2c178a7c2b01cd5b5ad5678ff8
PushkinaArina <pushkinaarina1@bk.ru>
2025-10-05 09:08:51 +0300

    Правки Booking. Исправление dto.

---
+++ src/main/java/ru/practicum/shareit/item/SimpleItemDto.java
@@ -0,0 +1,13 @@
+package ru.practicum.shareit.item;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SimpleItemDto {
+    private Long itemId;
+    private String name;
+}
