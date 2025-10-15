4b0c14eccede9c3a5d132a302f216199c13b6bb2
PushkinaArina <pushkinaarina1@bk.ru>
2025-10-16 00:42:39 +0300

    Postman проходит тесты!

--- shareIt-server/src/test/java/ShareItTests.java
+++ shareIt-server/src/test/java/ShareItTests.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
+@ComponentScan(basePackages = "ru.practicum.shareit")
 class ShareItTests {
 
     @Test
