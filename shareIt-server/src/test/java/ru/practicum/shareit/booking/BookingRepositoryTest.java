b01b36997b42a5eb75b676c9c0cc65d65f4c6e33
PushkinaArina <pushkinaarina1@bk.ru>
2025-10-16 00:42:39 +0300

    Postman проходит тесты!

--- shareIt-server/src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
+++ shareIt-server/src/test/java/ru/practicum/shareit/booking/BookingRepositoryTest.java
@@ -15,7 +15,6 @@ import ru.practicum.shareit.user.User;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
